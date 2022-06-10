f0c2c86d079257c3eee9bbc583e790b03b117953
yacine-boucetta <yacine.boucetta@laplateforme.io>
2022-06-10 12:31:18 +0200

    .

---
+++ script2.js
@@ -0,0 +1,57 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+//-----------------user friendly password--------
+let password = document.querySelector("[name='password']")
+let p = document.querySelectorAll("p")
+
+
+let regexLowerCase = /[a-z]{1,}/
+let regexUpperCase = /[A-Z]{1,}/
+let regexNumber = /[0-9]{1,}/
+//let regexSpeCharac = /^[^@&".()!_$*€£`+=\/;?#]+$/
+let passwordMinLength = /^[A-Za-z0-9\d@$!%*?&].{6,}$/
+
+password.addEventListener("keyup", () => {
+
+console.log(password.value)
+
+    if (regexLowerCase.test(password.value) == true) {
+        // on mondifie le style avec la commande style.color
+        p[0].style.color = 'aqua'
+
+    } else {
+
+        p[0].style.color = 'grey'
+    }
+
+
+    if (regexUpperCase.test(password.value) == true) {
+
+        // on mondifie le style avec la commande style.color
+        p[1].style.color = 'aqua'
+
+    } else {
+
+        p[1].style.color = 'grey'
+    }
+
+
+    if (passwordMinLength.test(password.value) == true) {
+        console.log(passwordMinLength.test(password.value))
+        // on mondifie le style avec la commande style.color
+        p[2].style.color = 'aqua'
+
+    } else {
+        p[2].style.color = 'grey'
+    }
+
+    if (regexNumber.test(password.value) == true) {
+
+        p[3].style.color = 'aqua'
+
+    } else {
+
+        p[3].style.color = 'grey'
+    }
+}
+)})
--- template.php
+++ template.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="style.css" rel="stylesheet" type="text/css">
     <script src="script.js"></script>
+    <script src="script2.js"></script>
     <title>Document</title>
 </head>
 <body>
